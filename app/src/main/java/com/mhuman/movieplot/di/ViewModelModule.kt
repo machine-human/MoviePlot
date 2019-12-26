0c2c76d97623d5b71c305266f2c049360e6a0d7c
machine-human <dhwltn02@gmail.com>
2019-12-26 16:23:58 +0900

    Di 모듈 추가 - ViewModel

--- app/src/main/java/com/mhuman/movieplot/di/ViewModelModule.kt
+++ app/src/main/java/com/mhuman/movieplot/di/ViewModelModule.kt
@@ -1,6 +1,6 @@
 package com.mhuman.movieplot.di
 
-import com.mhuman.movieplot.ui.category.CategoryViewModel
+import com.mhuman.movieplot.ui.detail.DetailViewModel
 import com.mhuman.movieplot.ui.favorite.FavoriteViewModel
 import com.mhuman.movieplot.ui.movie.MovieViewModel
 import com.mhuman.movieplot.ui.setting.SettingViewModel
@@ -8,8 +8,8 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
-    viewModel { MovieViewModel(get()) }
-    viewModel { CategoryViewModel() }
-    viewModel { FavoriteViewModel() }
-    viewModel { SettingViewModel() }
+    viewModel { FavoriteViewModel(get()) }
+    viewModel { SettingViewModel(get()) }
+    viewModel { DetailViewModel(get(), get(), get()) }
+    viewModel { MovieViewModel(get(), get(), get(), get()) }
 }
