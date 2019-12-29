6e1ca651859cd33e7cb678dfe2b7a52fffd49845
machine-human <dhwltn02@gmail.com>
2019-12-29 23:41:08 +0900

    kakaolink 공유 후 클릭시 처리 내용 추가

--- app/src/main/java/com/mhuman/movieplot/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/mhuman/movieplot/ui/detail/DetailActivity.kt
@@ -1,5 +1,6 @@
 package com.mhuman.movieplot.ui.detail
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.lifecycle.Observer
@@ -26,6 +27,7 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
 class DetailActivity : BaseActivity<ActivityDetailBinding>(R.layout.activity_detail) {
+
     private val viewModel by viewModel<DetailViewModel>()
     private val recyclerViewAdapter: DetailRecyclerViewAdapter by lazy { DetailRecyclerViewAdapter() }
     private val movieInfoList: MutableList<MovieInfoList> = mutableListOf()
@@ -58,7 +60,7 @@ class DetailActivity : BaseActivity<ActivityDetailBinding>(R.layout.activity_det
         text_view_movie_title_content.apply {
             text = intent.getStringExtra(EXTRA_MOVIE_TITLE)
         }
- 
+
         card_view_movie_title.setOnClickListener {
             browse(BuildConfig.NAVER_SEARCH_URL + intent.getStringExtra(EXTRA_MOVIE_TITLE))
         }
@@ -94,6 +96,12 @@ class DetailActivity : BaseActivity<ActivityDetailBinding>(R.layout.activity_det
                     LinkObject.newBuilder()
                         .setWebUrl(KAKAO_BASE_LINK)
                         .setMobileWebUrl(KAKAO_BASE_LINK)
+                        .setAndroidExecutionParams(
+                            "movieId=" + intent.getIntExtra(EXTRA_MOVIE_ID, 0)
+                                    + "&movieTitle=" + intent.getStringExtra(EXTRA_MOVIE_TITLE)
+                                    + "&moviePosterPath=" + intent.getStringExtra(EXTRA_MOVIE_POSTER_PATH)
+                                    + "&movieOverView=" + intent.getStringExtra(EXTRA_MOVIE_OVERVIEW)
+                        )
                         .build()
                 )
                     .setDescrption(intent.getStringExtra(EXTRA_MOVIE_OVERVIEW))
@@ -103,8 +111,12 @@ class DetailActivity : BaseActivity<ActivityDetailBinding>(R.layout.activity_det
                     "앱에서 바로 확인", LinkObject.newBuilder()
                         .setWebUrl(KAKAO_BASE_LINK)
                         .setMobileWebUrl(KAKAO_BASE_LINK)
-                        .setAndroidExecutionParams("123")
-                        .build()
+                        .setAndroidExecutionParams(
+                            "movieId=" + intent.getIntExtra(EXTRA_MOVIE_ID, 0)
+                                    + "&movieTitle=" + intent.getStringExtra(EXTRA_MOVIE_TITLE)
+                                    + "&moviePosterPath=" + intent.getStringExtra(EXTRA_MOVIE_POSTER_PATH)
+                                    + "&movieOverView=" + intent.getStringExtra(EXTRA_MOVIE_OVERVIEW)
+                        ).build()
                 )
             )
             .build()
